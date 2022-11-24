278945e64a452657209b29e2a4de0273cd4eb90e
Jonas Lindemann <jonas.lindemann@gmail.com>
2022-11-24 07:57:24 +0100

    More work on oop

--- general_oop/main.f90
+++ general_oop/main.f90
@@ -1,57 +1,140 @@
 program myfortran
 
-    ! --- Put use statements here
-    !
     use shape_classes
 
-    implicit none
+implicit none
 
-    type ShapeArray
-        class(Shape), pointer :: element
-    end type
+type SimpleShapeArray
+    type(Shape), pointer :: element
+end type
 
-    integer :: i
+type ShapeArray
+    class(Shape), pointer :: element
+end type
 
-    type(Shape) :: s
-    type(Shape), pointer :: s2
-    type(Square) :: sq
-    type(Circle) :: c
+integer :: i
 
-    class(Square), pointer :: p_square
-    class(Circle), pointer :: p_circle
-    class(Shape), pointer :: p_shape
+type(Shape) :: s
+type(Shape), allocatable :: s2
+type(Square) :: sq
+type(Circle) :: c
 
-    type(ShapeArray), allocatable :: shapes(:)
+! Instantiating a Particle object
 
-!    call s % init()
-!    call s % print()
-!    call s % set_pos(2.0, 2.0)
-!    call s % print()
+type(Particle) :: p
 
-!    allocate(s2)
+class(Square), pointer :: p_square
+class(Circle), pointer :: p_circle
+class(Shape), pointer :: p_shape
+type(Shape), pointer :: p_simple_shape
+type(SimpleShapeArray), allocatable :: simple_shapes(:)
+type(ShapeArray), allocatable :: shapes(:)
+type(Shape) :: static_shapes(20)
+type(Shape), allocatable :: dyn_shapes(:)
 
-!    call s2 % init()
-!    call s2 % print()
+class(Triangle), pointer :: p_triangle
+class(Point), pointer :: p_p0
+class(Point), pointer :: p_p1
+class(Point), pointer :: p_p2
 
-!    deallocate(s2)
+allocate(p_triangle)
+call p_triangle % init()
 
-!    call sq % init()
-!    call sq % print()
+p_p0 => p_triangle % get_point(1)
+p_p1 => p_triangle % get_point(1)
+p_p2 => p_triangle % get_point(1)
 
-!    call c % init()
-!    call c % print()
+call p_p0 % set_pos(0.0, 0.0)
+call p_p1 % set_pos(1.0, 0.0)
+call p_p2 % set_pos(1.0, 1.0)
 
-    allocate(shapes(20))
+call p_triangle % print()
 
-    do i = 1, 20
-        allocate(p_square)
-        shapes(i) % element => p_square
-    end do
+stop
 
-    do i = 1, 20
-        p_shape => shapes(i) % element
-        call p_shape % print()
-    end do
+allocate(p_square)
+allocate(p_circle)
+
+call p_square % init()
+call p_circle % init()
+
+print*, '-----'
+
+p_shape => p_square
+
+call p_shape % print()
+
+p_shape => p_circle
+
+call p_shape % print()
+
+print*, '-----'
+
+stop
+
+do i=1,20
+    call static_shapes(i) % init()
+    call static_shapes(i) % print()
+end do
+
+allocate(dyn_shapes(20))
+
+do i=1,20
+   call dyn_shapes(i) % init()
+   call dyn_shapes(i) % print()
+end do
+
+deallocate(dyn_shapes)
+
+allocate(simple_shapes(20))
+
+do i=1,20
+    allocate(p_simple_shape)
+    call p_simple_shape % init()
+    simple_shapes(i) % element => p_simple_shape
+end do
+
+do i=1,20
+    p_simple_shape => simple_shapes(i) % element
+    call p_simple_shape % print()
+end do
+
+do i=1,20
+    p_simple_shape => simple_shapes(i) % element
+    deallocate(p_simple_shape)
+end do
+
+deallocate(simple_shapes)
+
+s = Shape()
+!call s % init()
+call s % print()
+call s % set_pos(2.0, 2.0)
+call s % print()
+
+allocate(s2)
+
+call s2 % init()
+call s2 % print()
+
+deallocate(s2)
+
+call sq % init()
+call sq % print()
+call c % init()
+call c % print()
+
+allocate(shapes(20))
+
+do i = 1, 20
+    allocate(p_square)
+    shapes(i) % element => p_square
+end do
+
+do i = 1, 20
+    p_shape => shapes(i) % element
+    call p_shape % print()
+end do
 
 contains
 
--- general_oop/shape_classes.f90
+++ general_oop/shape_classes.f90
@@ -4,6 +4,12 @@ implicit none
 
 ! --- Variable declarations here
 
+type Particle
+    real :: pos(3)
+    real :: vel(3)
+    real :: mass
+end type
+
 type Shape
     private
     real :: pos(2)
@@ -11,7 +17,12 @@ contains
     procedure :: init => shape_init
     procedure :: set_pos => shape_set_pos
     procedure :: print => shape_print
+    final :: shape_destructor
 end type
+
+interface Shape
+    module procedure shape_constructor
+end interface
 
 type, extends(Shape) :: Square
     private
@@ -30,6 +41,29 @@ contains
     procedure :: set_radius => circle_set_radius
 end type
 
+type Point
+private
+    real :: m_x, m_y
+contains
+    procedure :: init => point_init
+    procedure :: set_pos => point_set_pos
+    procedure :: print => point_print
+end type
+
+type PointElement
+    class(Point), pointer :: p_element
+end type
+
+type Triangle
+private
+    type(PointElement) :: m_points(3)
+contains
+    procedure :: init => triangle_init
+    procedure :: get_point => triangle_get_point
+    procedure :: print => triangle_print
+end type
+
+
 ! -----------------------------------------------------
 
 contains
@@ -38,16 +72,33 @@ contains
 
 subroutine shape_init(this)
 
-    class(Shape), intent(out) :: this
+    class(Shape) :: this
 
     print*, 'shape_init()'
     this % pos = (/ 0.0, 0.0 /)
 
 end subroutine
 
+function shape_constructor() result(shape_inst)
+
+    type(Shape) :: shape_inst
+
+    print*, 'shape_constructor()'
+    call shape_init(shape_inst)
+
+end function
+
+subroutine shape_destructor(this)
+
+    type(Shape) :: this
+
+    print*, 'shape_destructor()'
+
+end subroutine
+
 subroutine shape_set_pos(this, x, y)
 
-    class(Shape), intent(out) :: this
+    class(Shape) :: this
     real, intent(in) :: x
     real, intent(in) :: y
 
@@ -58,7 +109,7 @@ end subroutine
 
 subroutine shape_print(this)
 
-    class(Shape), intent(in) :: this
+    class(Shape) :: this
 
     print*, 'shape_print()'
     print*, 'x = ', this % pos(1)
@@ -70,7 +121,7 @@ end subroutine
 
 subroutine square_init(this)
 
-    class(Square), intent(out) :: this
+    class(Square) :: this
 
     print*, 'square_init()'
 
@@ -83,7 +134,7 @@ end subroutine
 
 subroutine square_print(this)
 
-    class(Square), intent(in) :: this
+    class(Square) :: this
 
     print*, 'square_print()'
     print*, 'side = ', this % side
@@ -95,7 +146,7 @@ end subroutine
 
 subroutine circle_init(this)
 
-    class(Circle), intent(out) :: this
+    class(Circle) :: this
 
     print*, 'circle_init()'
     call shape_init(this)
@@ -115,7 +166,7 @@ end subroutine
 
 subroutine circle_print(this)
 
-    class(Circle), intent(in) :: this
+    class(Circle) :: this
 
     print*, 'cirlce_print()'
     print*, 'radius = ', this % radius
@@ -123,4 +174,78 @@ subroutine circle_print(this)
 
 end subroutine
 
+! -----------------------------------------------------
+
+subroutine point_init(this, x, y)
+
+    class(Point) :: this
+    real :: x, y
+
+    this % m_x = x
+    this % m_y = y
+
+end subroutine
+
+subroutine point_set_pos(this, x, y)
+
+    class(Point) :: this
+    real x, y
+
+    this % m_x = x
+    this % m_y = y
+
+end subroutine
+
+subroutine point_print(this)
+
+    class(Point) :: this
+
+    print*, 'Point'
+    print*, this % m_x, this % m_y
+
+end subroutine
+
+! -----------------------------------------------------
+
+subroutine triangle_init(this)
+
+    class(Triangle) :: this
+    class(Point), pointer :: p_point
+
+    integer :: i
+
+    do i=1,3
+        allocate(p_point)
+        call p_point % init(0.0, 0.0)
+        this % m_points(i) % p_element => p_point
+    end do
+
+end subroutine
+
+function triangle_get_point(this, idx) result(p_inst)
+
+    class(Triangle) :: this
+    class(Point), pointer :: p_inst
+    integer :: idx
+
+    p_inst => this % m_points(idx) % p_element
+
+end function
+
+subroutine triangle_print(this)
+
+    class(Triangle) :: this
+    class(Point), pointer :: p_point
+    integer :: i
+
+    print*, 'Triangle'
+
+    do i=1,3
+        call this % m_points(i) % p_element % print()
+    end do
+
+end subroutine
+
 end module shape_classes
+
+
